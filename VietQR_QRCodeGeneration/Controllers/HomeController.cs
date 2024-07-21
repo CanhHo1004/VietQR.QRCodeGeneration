d8f631e948a79d53da545aed4569b063fe23bb33
CanhHo1004 <minhcanh1004@gmail.com>
2024-07-21 21:31:56 +0700

    Update portal for system

--- VietQR_QRCodeGeneration/Controllers/HomeController.cs
+++ VietQR_QRCodeGeneration/Controllers/HomeController.cs
@@ -1,8 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using GSF;
+using QRCoder;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Web.Mvc;
+using System.Web.WebPages;
+using VietQR_QRCodeGeneration.Ultil;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace VietQR_QRCodeGeneration.Controllers
 {
@@ -10,9 +15,56 @@ namespace VietQR_QRCodeGeneration.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
-
             return View();
+        }
+
+        public ActionResult Create(string bankcode = "", string accnumber = "", string amount = "", string content = "")
+        {
+            string base64QRCode = "";
+            decimal d_amount = 0;
+            if (bankcode.IsNullOrWhiteSpace() || bankcode.IsEmpty())
+            {
+                ViewBag.ErrorMessage = "Không được để trống mã ngân hàng!";
+                return View("Index");
+            }
+            if(accnumber.IsNullOrWhiteSpace() || accnumber.IsEmpty())
+            {
+                ViewBag.ErrorMessage = "Không được để trống số tài khoản!";
+                return View("Index");
+            }
+            if (!(amount.IsNullOrWhiteSpace() || amount.IsEmpty()))
+            {
+                try
+                {
+                    d_amount = decimal.Parse(amount);
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.ErrorMessage = "Số tiền nhập không đúng định dạng!";
+                    return View("Index");
+                }
+            }
+            
+            using (QRCodeGenerator qrGenerator = new QRCodeGenerator())
+            {
+                string qrcontent = Function.createQRCodeContent(bankcode, accnumber, d_amount, content);
+
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(qrcontent, QRCodeGenerator.ECCLevel.Q);
+                using (QRCode qrCode = new QRCode(qrCodeData))
+                {
+                    using (Bitmap qrCodeImage = qrCode.GetGraphic(3))
+                    {
+                        using (MemoryStream memoryStream = new MemoryStream())
+                        {
+                            qrCodeImage.Save(memoryStream, ImageFormat.Png);
+                            byte[] qrCodeBytes = memoryStream.ToArray();
+                            base64QRCode = Convert.ToBase64String(qrCodeBytes);
+                            ViewBag.QrCodeImage = "data:image/png;base64," + base64QRCode;
+                        }
+                    }
+                }
+            }
+            return View("Index");
         }
     }
 }
--- VietQR_QRCodeGeneration/Controllers/QRCodeGenerationController.cs
+++ VietQR_QRCodeGeneration/Controllers/QRCodeGenerationController.cs
@@ -34,7 +34,7 @@ namespace VietQR_QRCodeGeneration.Controllers
 
             using (QRCodeGenerator qrGenerator = new QRCodeGenerator())
             {
-                var qrContent = createQRCodeContent(bankCode, accountNumber, amount, message);
+                var qrContent = Function.createQRCodeContent(bankCode, accountNumber, amount, message);
                 QRCodeData qrCodeData = qrGenerator.CreateQrCode(qrContent, QRCodeGenerator.ECCLevel.Q);
                 using (QRCode qrCode = new QRCode(qrCodeData))
                 {
@@ -49,53 +49,7 @@ namespace VietQR_QRCodeGeneration.Controllers
                     }
                 }
             }
-
             return new Response { resultCode = (int)HttpStatusCode.OK, resultString = "Success", resultData = base64QRCode };
-        }
-
-        private string createQRCodeContent(string bankCode, string accountNumber, decimal amount = 0, string message = "")
-        {
-            var contentQRCode = "";
-            var amount_str = "";
-            var message_str = "";
-
-            var payload_format_indicator = "000201";    //(1) Payload mặc định 
-            var point_of_initiation_method = "010211";  //(2) Định dạng mặc định là QR tĩnh 
-            var country_code = "5802VN"; //Mã quốc gia
-            var money_country_code = "5303704"; //(8) Đơn vị tiền tệ VNĐ
-            var payload_type = "0208QRIBFTTA"; //(7) Phương thức giao dịch: Chuyển tiền đến số tài khoản
-            var merchant_account_info_template = "0010A000000727"; //(4) khai báo template của thông tin tài khoản
-            var payment_info = String.Format("00{0}{1}01{2}{3}", bankCode.Length.ToString("00"), bankCode, accountNumber.Length.ToString("00"), accountNumber); //(6) Thông tin ngân hàng và tài khoản thụ hưởng
-            var len_payment_info = String.Format("01{0}", payment_info.Length.ToString("00")); //(5) Khai báo độ dài của thông tin ngân hàng + tài khoản
-            var len_account_payment_info = String.Format("38{0}", (merchant_account_info_template + len_payment_info + payment_info + payload_type).Length.ToString("00")); //(3)Khai báo thông tin và tổng độ dài của thông tin thụ hưởng
-            var crc_place_holder = "6304";
-
-            if (amount != 0)
-            {
-                amount_str += String.Format("54{0}{1}", amount.ToString().Length.ToString("00"), amount);
-            }
-            if (!message.Equals(""))
-            {
-                message_str += String.Format("62{0}08{1}{2}", (message.Length + 4).ToString("00"), (message.Length).ToString("00"), message);
-            }
-
-            contentQRCode += payload_format_indicator + 
-                point_of_initiation_method + 
-                len_account_payment_info + 
-                merchant_account_info_template + 
-                len_payment_info + payment_info + 
-                payload_type + 
-                money_country_code +
-                amount_str +
-                country_code +
-                message_str +
-                crc_place_holder;
-
-            var crc = Function.CRC16Trans(contentQRCode);
-
-            contentQRCode += crc;
-
-            return contentQRCode;
         }
     }
 }
